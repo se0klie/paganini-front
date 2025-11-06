de0f8a77f04db850897898d4803deaea9f71cebc
jojusuar <jojusuar@espol.edu.ec>
2025-11-06 13:49:59 -0500

    signup api call and success modal

--- src/pages/Login/Signup.jsx
+++ src/pages/Login/Signup.jsx
@@ -1,14 +1,17 @@
-import { Grid, Box, Button, Divider, TextField, Typography, Stack, Tooltip, IconButton } from "@mui/material";
+import { Grid, Box, Button, Divider, TextField, Typography } from "@mui/material";
 import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import React, { useState, useEffect, useRef } from "react";
+import ErrorModal from "../../shared components/Modals.jsx";
+import { SuccessModal } from "../../shared components/Modals";
+import { useState } from "react";
+import api from "../../axios";
 import '../../style.css';
-import PasswordField from "../../shared components/Inputs";
-import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
 export default function SignupPage() {
     const { login } = useAuth();
     const navigate = useNavigate();
+    const [openError, setOpenError] = useState(false);
+    const [openSuccess, setOpenSuccess] = useState(false);
     const [userData, setUserData] = useState({
         email: '',
         name: '',
@@ -26,6 +29,62 @@ export default function SignupPage() {
         { id: "password", label: "Contraseña", placeholder: "Tu contraseña" },
         { id: "confirmPassword", label: "Confirmar contraseña", placeholder: "Repite la contraseña" }
     ];
+
+    async function handleSignup() {
+        try {
+            const { name, lastname, email, phone, password, confirmPassword } = userData;
+
+            const nameRegex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+            const phoneRegex = /^09\d{8}$/;
+            const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?]).{8,}$/;
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+            if (!nameRegex.test(name) || !nameRegex.test(lastname)) {
+                alert("Nombre y apellido no pueden estar vacíos ni contener números o símbolos.");
+                return;
+            }
+
+            if (!emailRegex.test(email)) {
+                alert("Ingrese un correo electrónico válido.");
+                return;
+            }
+
+            if (!phoneRegex.test(phone)) {
+                alert("El teléfono debe tener 10 dígitos y comenzar con 09.");
+                return;
+            }
+
+            if (!passwordRegex.test(password)) {
+                alert("La contraseña debe tener al menos 8 caracteres, una mayúscula, un número y un símbolo.");
+                return;
+            }
+
+            if (password !== confirmPassword) {
+                alert("Las contraseñas no son iguales.");
+                return;
+            }
+
+            const response = await api.post('/auth/signup', {
+                nombre: name,
+                apellido: lastname,
+                correo: email,
+                telefono: phone,
+                password: password
+            });
+            if (response.status === 200 || response.status === 201) {
+                setOpenSuccess(true);
+                setTimeout(() => {
+                    setOpenSuccess(false);
+                    navigate('/login');
+                }, 3000);
+            }
+
+        } catch (err) {
+            console.error("Signup error: ", err)
+            setOpenError(true);
+            return err
+        }
+    }
 
     return (
         <Box sx={{ p: 4, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: 'var(--color-primary)', minHeight: '100vh' }}>
@@ -59,10 +118,10 @@ export default function SignupPage() {
                                     fullWidth
                                     size="small"
                                     placeholder={field.placeholder}
-                                    type={field.label.toLowerCase().substring("contraseña") ? "password" : "text"}
+                                    type={field.label.toLowerCase().includes("contraseña") ? "password" : "text"}
                                     onChange={(e) => setUserData(prev => ({
                                         ...prev,
-                                        [id]: e.target.value
+                                        [field.id]: e.target.value
                                     }))}
                                 />
                             </Box>
@@ -83,9 +142,22 @@ export default function SignupPage() {
                             background: "var(--color-secondary-dark)",
                         },
                     }}
+                    onClick={handleSignup}
                 >
                     Registrarse
                 </Button>
+                <ErrorModal
+                    open={openError}
+                    onClose={() => setOpenError(false)}
+                    title="Error"
+                    message="Hubo un problema al registrarte. Inténtalo más tarde."
+                />
+                <SuccessModal
+                    open={openSuccess}
+                    onClose={() => setOpenSuccess(false)}
+                    title="Registrado!"
+                    message="Te redirigiremos para que inicies sesión."
+                />
             </Box>
         </Box>
     )
