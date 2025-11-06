b7242981e2ae2da014f34109358481e6677bb99e
hailie <142462676+se0klie@users.noreply.github.com>
2025-11-06 18:59:19 -0500

    add modals and disabled auth for dev

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,14 +3,13 @@ import Cookies from "js-cookie";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null); 
+  const [user, setUser] = useState(null);
 
   const login = (userData) => setUser(userData);
   const logout = () => {
     Cookies.remove('accessToken', { path: "/" });
     Cookies.remove('refreshToken', { path: "/" });
     setUser(null)
-    
   };
 
   return (
